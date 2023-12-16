2445c604b9b35517ffd972aceff1944c2e828faa
Santan21 <lesliegyamfi02@gmail.com>
2023-12-16 22:30:08 +0000

    startig the API: Handling GET requests

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 
+app.get('/api/v1/blog',  );
 
 
 
 
-
-const port = 3000;
+const port = 8001;
 app.listen(port, () => {
     console.log('App runs on port ${port}.. ')
 });

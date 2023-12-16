a7f37c097fadeea4bcc08462e8463fc22fb39e8b
Santan21 <lesliegyamfi02@gmail.com>
2023-12-16 23:47:21 +0000

    Handling POST requests

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const blog = JSON.parse(fs.readFileSync(`${__dirname}/assets/data/blog-data.json
 app.get('/api/v1/blog', (req, res) => {
     res.status(200).json({
         status: 'success',
-
+        data: {
+            blog
+        }
     });
+});
 
+app.post('/api/v1/blog', (req, res) => {
+    
 });
 
 

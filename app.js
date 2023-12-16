b850b65c46e8f498a9a3a052d4346caca82bedf0
Santan21 <lesliegyamfi02@gmail.com>
2023-12-16 23:06:59 +0000

    requiring 'fs' module

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const fs = require('fs');
 const express = require('express');
+
 const app = express();
 
+const blog = fs.readFileSync(`${__dirname}/assets/data/blog-data.json`)
 
 app.get('/api/v1/blog', (req, res) => {
     res.status(200);
-    
-} );
+
+});
 
 
 
